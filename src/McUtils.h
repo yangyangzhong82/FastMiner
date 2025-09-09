06771ea73a10affc60df475e09faab8d8720f10b
engsr6982 <pwxg286704@163.com>
2025-09-09 17:10:44 +0800

    refactor: clean up unnecessary includes and functions in McUtils.h

--- src/McUtils.h
+++ src/McUtils.h
@@ -3,21 +3,11 @@
 #include "ll/api/service/Bedrock.h"
 #include "mc/_HeaderOutputPredefine.h"
 #include "mc/deps/core/string/HashedString.h"
-#include "mc/deps/core/utility/MCRESULT.h"
-#include "mc/deps/game_refs/StackRefResult.h"
 #include "mc/deps/game_refs/WeakRef.h"
-#include "mc/external/render_dragon/frame_renderer/CommandContext.h"
-#include "mc/locale/I18n.h"
-#include "mc/locale/Localization.h"
 #include "mc/server/ServerLevel.h"
 #include "mc/server/commands/CommandBlockNameResult.h"
-#include "mc/server/commands/CommandContext.h"
 #include "mc/server/commands/CommandOrigin.h"
 #include "mc/server/commands/CommandOutput.h"
-#include "mc/server/commands/CommandOutputType.h"
-#include "mc/server/commands/CommandPermissionLevel.h"
-#include "mc/server/commands/CommandVersion.h"
-#include "mc/server/commands/CurrentCmdVersion.h"
 #include "mc/server/commands/MinecraftCommands.h"
 #include "mc/server/commands/PlayerCommandOrigin.h"
 #include "mc/server/commands/ServerCommandOrigin.h"
@@ -35,7 +25,6 @@
 #include "mc/world/level/chunk/ChunkSource.h"
 #include "mc/world/level/chunk/LevelChunk.h"
 #include "mc/world/level/dimension/Dimension.h"
-#include "mc/world/level/dimension/DimensionHeightRange.h"
 #include <algorithm>
 #include <ll/api/service/Bedrock.h>
 #include <ll/api/service/ServerInfo.h>
@@ -63,19 +52,6 @@ namespace mc_utils {
     throw std::runtime_error("dimension not found");
 }
 
-inline void executeCommand(const std::string& cmd, Player* player) {
-    auto& minecraftCommands = ll::service::getMinecraft()->mCommands;
-    if (!minecraftCommands) {
-        return;
-    }
-    CommandContext ctx = CommandContext(
-        cmd,
-        std::make_unique<PlayerCommandOrigin>(PlayerCommandOrigin(*player)),
-        CommandVersion::CurrentVersion()
-    );
-    minecraftCommands->executeCommand(ctx, true);
-}
-
 // IDA: v1.21.0
 [[nodiscard]] inline bool PlayerIsSneaking(Player& player) {
     return SynchedActorDataAccess::getActorFlag(player.getEntityContext(), ActorFlags::Sneaking);
@@ -95,28 +71,6 @@ inline void executeCommand(const std::string& cmd, Player* player) {
     }
     return it->canDestroySpecial(block);
 }
-
-[[nodiscard]] inline short GetDimensionMinHeight(Dimension& dim) {
-    auto& range = dim.mHeightRange.get();
-    return range.mMin;
-}
-
-[[nodiscard]] inline short GetDimensionMaxHeight(Dimension& dim) {
-    auto& range = dim.mHeightRange.get();
-    return range.mMax;
-}
-
-[[nodiscard]] inline bool IsChunkFullyLoaded(ChunkSource& chs, ChunkPos const& pos) {
-    if (!chs.isWithinWorldLimit(pos)) return true;
-    auto chunk = chs.getOrLoadChunk(pos, ::ChunkSource::LoadMode::None, true);
-    return chunk && static_cast<int>(chunk->mLoadState->load()) >= static_cast<int>(ChunkState::Loaded)
-        && !chunk->mIsEmptyClientChunk && chunk->mIsRedstoneLoaded;
-}
-
-[[nodiscard]] inline bool IsChunkFullLyoaded(BlockPos const& pos, BlockSource& bs) {
-    return IsChunkFullyLoaded(bs.getChunkSource(), ChunkPos(pos));
-}
-
 
 [[nodiscard]] inline BlockPos face2Pos(BlockPos const& sour, uchar face) {
     BlockPos dest = sour;
